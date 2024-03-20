a5899955c1173d47e577bad5868e7149e523148d
Chinchainis <antonio.sanchez.alcaraz.wii@gmail.com>
2024-03-20 22:49:36 +0100

    Implementada función con slicing
    Creo que no recorre la canción el número de veces correcta, he puesto anotaciones para estudiarlo

--- PruebasPDS.py
+++ PruebasPDS.py
@@ -50,19 +50,27 @@ def representar(fun1,fun2):
     plt.show()
     
 def comparar(fun1,fun2):
-    error=0
+    #fun1 canción a comparar, fun2 fragmento que comparamos
     sumerror = 0
+    #sumerror sumatoria del algoritmo básico
     pos = 0
+    #valor mínimo actual, indicaría qué canción tiene el menos error de comparación
     valmin = 0
-    listavalmin = 0
+    #Indica el número de veces que es de grande la canción en comparación con el fragmento
     print(fun1.shape[0] / fun2.shape[0])
-    for i in range(0, fun1.shape[0]):
-        sumerror += (fun2[pos] - fun1[i])**2
+    #recorref1 array de tamaño del fragmento para reducir tamaño de comparación
+    recorref1 = np.zeros(fun2.shape[0])
+    print(fun1.shape[0]-fun2.shape[0])
+    #el bucle va desde el principio de la canción (0) hasta el final menos el tamaño del fragmento, dado que comparamos en trozos
+    #del tamaño de fragmentos pues acabaría terminando en 32000 |_32000_|_32000_|_32000_|
+    for i in range(0, fun1.shape[0]-fun2.shape[0]):
+        #recorref1 empieza de 0 hasta el tamaño del fragmento 2, si es 32000 es un vector del 0 al 31999
+        recorref1 = fun1[i:fun2.shape[0]+i]
+        #sumatoria
+        sumerror += (fun2[pos] - recorref1[pos])**2
         if (pos == fun2.shape[0]-1):
-            #print("fun2 pos",fun2[pos])
-            #print("fun1 i",fun1[i])
-            #print("sumerror uni",(fun2[pos] - fun1[i])**2)
-            if (valmin==0):
+            #para que el valor mínimo se sustituya la primera vez aunque sea 0 188806
+            if (valmin==0): 
                 valmin = sumerror
             if (sumerror < valmin):
                 valmin = sumerror
