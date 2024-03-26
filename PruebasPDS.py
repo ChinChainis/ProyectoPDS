5552caffbbd9c30fb7e08633c3ddc8b9c9a54fd6
miguel24421 <miguel24@correo.ugr.es>
2024-03-26 20:07:02 +0100

    Incorporacion del codigo del conversor Se ha añadido y ajustado al codigo el codigo del conversor de mp3 a wav Se ha añadido a audios dos archivos .mp3 para testeo Se ha borrado el archivo .mp3 original por motivos de optimización del tamaño

--- PruebasPDS.py
+++ PruebasPDS.py
@@ -10,6 +10,23 @@ import os
 import pydub as pd #se usa para tener la función de transformar mp3 en wav
 import matplotlib.pyplot as plt
 import math
+#importar lo del  segmento de audio
+from pydub import AudioSegment
+
+
+#conda install -c conda-forge ffmpeg
+#o añadir desde el anaconda navigator enviroments el ffmpeg
+
+
+def mp3awav(ubicacion):
+    path = os.getcwd()
+    os.chdir(path + '/' + ubicacion)
+    for archivo in  os.listdir(os.getcwd()):
+        nombre, ext = os.path.splitext(archivo)
+        if ext == ".mp3":
+                mp3_sound = AudioSegment.from_mp3(archivo)
+                mp3_sound.export("{0}.wav".format(nombre), format="wav")
+    os.chdir(path)
 
 #a = open("test.txt")
 #print(a.read())
@@ -28,8 +45,13 @@ valmintotal = 0
 
 
 #ej, samplerate = sf.read(c)
-archivos = os.listdir(carpeta)
-numarch = len(os.listdir(carpeta))
+mp3awav(carpeta)
+archivos= os.listdir(carpeta)
+for archivo in os.listdir(carpeta):
+    nombre, ext = os.path.splitext(archivo)
+    if ext == ".mp3":
+        archivos.remove(nombre+".mp3")
+numarch = len(archivos)
 
 print(numarch)
 
@@ -50,49 +72,52 @@ def representar(fun1,fun2):
     plt.show()
     
 def comparar(fun1,fun2):
-    error=0
+    #fun1 canción a comparar, fun2 fragmento que comparamos
     sumerror = 0
+    #sumerror sumatoria del algoritmo básico
     pos = 0
+    #valor mínimo actual, indicaría qué canción tiene el menos error de comparación
     valmin = 0
-    listavalmin = 0
+    #Indica el número de veces que es de grande la canción en comparación con el fragmento
     print(fun1.shape[0] / fun2.shape[0])
-    for i in range(0, fun1.shape[0]):
-        for j in range(0, fun2.shape[0]):
-            #fun2 fragmento fun1 cancion
-            sumerror += (fun2[pos] - fun1[i])**2
-            if (pos == fun2.shape[0]-1):
-                #print("fun2 pos",fun2[pos])
-                #print("fun1 i",fun1[i])
-                #print("sumerror uni",(fun2[pos] - fun1[i])**2)
-                if (valmin==0):
-                    valmin = sumerror
-                if (sumerror < valmin):
-                    valmin = sumerror
-                #print("Val min actual: ",sumerror)
-                sumerror = 0
-                pos = 0
-            else:
-                pos+=1
+    #recorref1 array de tamaño del fragmento para reducir tamaño de comparación
+    recorref1 = np.zeros(fun2.shape[0])
+    print(fun1.shape[0]-fun2.shape[0])
+    #lista total de los valores obtenidos de recorrer una canción, se escoje el mínimo. 
+    #En teoría si es la misma canción el mínimo es 0
+    listavalores = np.zeros(fun1.shape[0]-fun2.shape[0])
+    #el bucle va desde el principio de la canción (0) hasta el final menos el tamaño del fragmento, dado que comparamos en trozos
+    #del tamaño de fragmentos pues acabaría terminando en 32000 |_32000_|_32000_|_32000_|
+    for i in range(0, fun1.shape[0]-fun2.shape[0]):
+        #recorref1 empieza de 0 hasta el tamaño del fragmento 2, si es 32000 es un vector del 0 al 31999
+        recorref1 = fun1[i:fun2.shape[0]+i]
+        #resta todos los elementos de ambos arrays
+        restaarrays = np.int64(np.subtract(fun2,recorref1))
+        #eleva cada elemento de los arrays a potencia de 2
+        potarrays = np.power(restaarrays,2)
+        #obtiene la sumatoria de todos los elementos del array
+        sumerror = np.sum(potarrays)
+        listavalores[i]=sumerror
+        sumerror = 0
+    #Sacamos el valor mínimo obtenido de recorrer la canción
+    valmin = listavalores[np.argmin(listavalores)]
     print("Valor minimo",valmin)
     return valmin
     #np.append(listavalmin,valmin/100)
     #print("lista total: ",listavalmin)
     
+#lista de valores mínimos de cada canción de la carpeta a analizar
+listavaldef = np.zeros(numarch-1)
 
 for i in range(0,numarch-1):
     print(archivos[i])
     Fs, funOG = wavfile.read(carpeta + '/' + archivos[i]) #Fs frecuencia de 16000 hercios 
     Fs, funFR = wavfile.read(fragmento)
-    nomcancion = ""
     valtemp = comparar(funOG,funFR)
-    if(valmintotal==0):
-        nomcancion = "Canción: "+archivos[i]
-        valmintotal=valtemp
-    elif(valtemp < valmintotal):
-        nomcancion = "Canción: "+archivos[i]
-        valmintotal=valtemp
-
-print("Canción coincidente: ",nomcancion, ". Valor: ",valmintotal)
+    #Se añade valor mínimo de una canción concreta a la lista de valores mínimos de cada canción a comparar
+    listavaldef[i] = valtemp
+valmintotaldef = listavaldef[np.argmin(listavaldef)]
+#Como guarda la posición del valor mínimo, pues corresponderá a esa canción coincidente
+nomcanciondef = archivos[np.argmin(listavaldef)]
+print("Resultado: ",nomcanciondef, ". Valor: ",valmintotaldef)
 #print(os.listdir("audios")[1])
-
-
--- audiomp3wav.py
+++ audiomp3wav.py
@@ -5,24 +5,34 @@ import pandas
 import sys
 import scipy as sc #procesamiento, para transformada fourier, interpolacion etc
 from scipy.io import wavfile
-#import soundfile as sf
+import soundfile as sf
 import os
 import pydub as pd #se usa para tener la función de transformar mp3 en wav
 import matplotlib.pyplot as plt
 import math
-#IMPORTANTE
-#importar solo el segmento de audio
+#importar lo del  segmento de audio
 from pydub import AudioSegment
 
-#IMPORTANTE
-#si no os funciona seguramente sea por ffmpeg
+
 #conda install -c conda-forge ffmpeg
-#o añadir desde el anaconda navigator => enviroments => el ffmpeg
+#o añadir desde el anaconda navigator enviroments el ffmpeg
 
 
-src = "kieran.mp3"
-dst = "test.wav"
+def mp3awav(ubicacion):
+    path = os.getcwd()
+    os.chdir(path + '/' + ubicacion)
+    for archivo in  os.listdir(os.getcwd()):
+        nombre, ext = os.path.splitext(archivo)
+        if ext == ".mp3":
+                mp3_sound = AudioSegment.from_mp3(archivo)
+                mp3_sound.export("{0}.wav".format(nombre), format="wav")
+    os.chdir(path)
 
-# convert mp3 to wav                                                            
-sound = AudioSegment.from_mp3(src) 
-sound.export(dst, format="wav")
+
+mp3awav("audios")
+archivos= os.listdir("audios")
+for archivo in os.listdir("audios"):
+    nombre, ext = os.path.splitext(archivo)
+    if ext == ".mp3":
+        archivos.remove(nombre+".mp3")
+print(len(archivos))
